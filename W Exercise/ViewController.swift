a91e99feaa2e90421b456418d94695699116c0fe
Joshua Onabanjo <joshua@memrise.com>
2016-11-29 16:23:13 +0000

    Weird print
    Testing commit

--- W Exercise/ViewController.swift
+++ W Exercise/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Insert 1000 lines of code")
+        print("Hacking...")
     }
 
     override func didReceiveMemoryWarning() {
